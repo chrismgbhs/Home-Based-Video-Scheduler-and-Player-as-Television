c225af94690e7a7f51381e4fbaf7824fe429589f
chrismgbhs <chrisboen8@gmail.com>
2026-03-22 16:44:47 +0800

    Add project files.

---
+++ Home-Based Video Scheduler and Player as Television/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Home_Based_Video_Scheduler_and_Player_as_Television
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
